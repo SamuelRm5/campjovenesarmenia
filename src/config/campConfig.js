bcee858bcff200ba4b8cd63c1f8c5430c41c7f67
Samuel Rodriguez Mazuera <86493107+SamuelRm5@users.noreply.github.com>
2025-09-11 11:06:24 -0500

    Update campConfig.js

--- src/config/campConfig.js
+++ src/config/campConfig.js
@@ -7,7 +7,7 @@ export const CAMP_CONFIG = {
     campStart: "2025-10-10T08:00:00",
     campEnd: "2025-10-13T18:00:00",
     paymentDeadline: "2025-09-28T23:59:59", // Fecha límite de pago
-    registrationDeadline: "2025-09-10T23:59:59", // Fecha límite de inscripción (Sin cupos)
+    registrationDeadline: "2025-09-28T23:59:59", // Fecha límite de inscripción (Sin cupos)
   },
 
   // Información del campamento
